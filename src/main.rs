64f49db1aa742ca2ae754ba3cd9d5506f3cefd39
Andreas Stocker <shadowstep7@gmail.com>
2023-01-21 17:00:06 +0100

    use constants for readability, pressing button lights green light

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,24 @@ global_asm!(include_str!("../start.s"));
 
 const CSR_BASE: u32 = 0xe0000000;
 
+const LED_RED: u32 = 0x6800;
+const LED_BLUE: u32 = 0x6808;
+const LED_GREEN: u32 = 0x6804;
+const LED_RAW: u32 = 0x6810;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn csr_write_simple(val: u32, addr: u32) {
+fn mem_read(addr: u32) -> u32 {
+    unsafe {
+        let pointer: *mut u32 = (CSR_BASE + addr) as *mut u32;
+        read_volatile(pointer)
+    }
+}
+
+fn mem_write(addr: u32, val: u32) {
     unsafe {
         let pointer: *mut u32 = (CSR_BASE + addr) as *mut u32;
         write_volatile(pointer, val);
@@ -24,13 +36,15 @@ fn csr_write_simple(val: u32, addr: u32) {
 
 #[no_mangle]
 extern "C" fn main() {
-    /* This write turns off blink/rainbow modes */
-    csr_write_simple(0, 0x6810);
+    mem_write(LED_RAW, 0);
 
-    // RGB blue
-	csr_write_simple(255, 0x6808);
-    // RGB red
-	csr_write_simple(0, 0x6800);
+    mem_write(LED_RED, 0);
+    mem_write(LED_BLUE, 0);
+
+    loop {
+        let val = if mem_read(0x8800) != 0 { 0 } else { 255 };
+        mem_write(LED_GREEN, val);
+    }
 }
 
 #[no_mangle]
